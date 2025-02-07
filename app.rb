b0338ec8da690f460e2a5a4186d39aa115bb3dd0
Maia Rowe <mrowe7000@icloud.com>
2025-02-07 08:26:40 +0000

    app method updates

--- app.rb
+++ app.rb
@@ -5,10 +5,6 @@ get("/") do
   redirect("/add")
 end
 
-get "/add" do
-  erb :add_form
-end
-
 get("/ad") do
   erb(:add_form)
 end
@@ -21,28 +17,34 @@ get("/wizard_add") do
 end
 
 get("/subtract") do
-  erb(:sub_form)
+  erb(:subtraction_form)
 end
 
 get("/wizard_subtract") do
   @first_num = params.fetch("first_num").to_f
   @second_num = params.fetch("second_num").to_f
-
+  @result = @first_num - @second_num
   erb(:sub_result)
 end
 
 get("/multiply") do
-  erb(:multiplication_form)
+  erb(:mult_form)
 end
 
 get("/wizard multiply") do
+  @first_num = params.fetch("first_num").to_f
+  @second_num = params.fetch("second_num").to_f
+  @result = @first_num * @second_num
   erb(:mult_result)
 end
 
 get("/divide") do
-  erb(:div_form)
+  erb(:divform)
 end
 
 get("/wizard_divide") do
+  @first_num = params.fetch("first_num").to_f
+  @second_num = params.fetch("second_num").to_f
+  @result = @first_num / @second_num
   erb(:div_result)
 end
